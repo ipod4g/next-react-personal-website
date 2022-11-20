3fd1727ee8bc808b15b96368439e4a9ee22fe258
romanbezushko <r.bezushko93@gmail.com>
2022-11-20 17:38:28 +0200

    created articles cards design

--- components/Posts/PostItem.jsx
+++ components/Posts/PostItem.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 function PostItem({post}) {
   const {title, thumbnailUrl, date, description, postSlug } = post;
 
   return (
     <Link href={`/posts/${postSlug}`}>
-      <div className='mb-8 w-full'>
-        <h4>{title}</h4>
+      <article className='flex-blog-cards py-6 px-4 border-2 mb-6 cursor-pointer rounded-md hover:shadow-md'>
+        <div className='w-full h-44 relative mb-2'>
+          <Image 
+            className='w-full'
+            src={post.thumbnailUrl}
+            alt='portfolio'
+            layout='fill'
+            objectFit='contain'
+          />
+        </div>
+      
+        <h4 className='font-bold'>{title}</h4>
         <p>{description}</p>
-      </div>
+      </article>
     </Link>
   );
 }
